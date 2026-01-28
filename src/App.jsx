cca568bbbfedfc0fa9e4bc3bddd0c2dbd5670da1
Chittansh <chittansh.p@somaiya.edu>
2026-01-28 18:45:47 +0530

    Day 1: Initial React setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>AI Sign Language Platform</h1>
+      <p>Built by Chittansh</p>
+    </div>
+  );
+}
+
+export default App;
+
